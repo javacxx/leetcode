4bec06e6fefab3109b4f849efedb08b1d07b7fd7
javacxx <xx.chen@foxmail.com>
2019-09-18 15:47:47 +0800

    validate int overflow

--- src/main/java/com/chen/array/ArrayAddFunc.java
+++ src/main/java/com/chen/array/ArrayAddFunc.java
@@ -13,7 +13,7 @@ public class ArrayAddFunc {
     private int size = 0;
 
     public boolean add(int element) {
-        ensureCapacity(size + 1);
+        ensureCapacity(plusInt(size, 1));
         elements[size++] = element;
         return true;
     }
@@ -21,35 +21,58 @@ public class ArrayAddFunc {
     private void ensureCapacity(int minCapacity) {
        // mod ++
         if (minCapacity > capacity) {
-            grow(minCapacity);
+            growCapacity(minCapacity);
         }
     }
 
-    private static final int MAX_ARRAY_SIZE = Integer.MAX_VALUE - 8;
-    private void grow (int minCapacity) {
-        if (minCapacity > MAX_ARRAY_SIZE) {
-            throw new OutOfMemoryError("array length over");
-        }
+    private void growCapacity(int minCapacity) {
         int newCapacity ;
         do {
-            newCapacity = capacity << 1;
-            if (newCapacity > MAX_ARRAY_SIZE) {
-                newCapacity = MAX_ARRAY_SIZE;
+            if (validatePlusOverflow(capacity, capacity)) {
+                newCapacity = Integer.MAX_VALUE;
+            } else {
+                newCapacity = capacity << 1;
             }
-        } while (newCapacity < minCapacity);
+        } while (newCapacity < minCapacity && newCapacity < Integer.MAX_VALUE);
 
         elements = Arrays.copyOf(elements, newCapacity);
         capacity = newCapacity;
     }
 
-    public static void main(String[] args) {
-        ArrayAddFunc arrayAddFunc = new ArrayAddFunc();
 
-        for (int i = 1; i<100; i++) {
-            arrayAddFunc.add(i);
+    private int plusInt(int a, int b) {
+        if (validatePlusOverflow(a, b)) {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.append(a)
+                    .append(" plus ")
+                    .append(b)
+                    .append(" overflow!");
+
+            throw new OutOfMemoryError(stringBuilder.toString());
         }
-        System.out.println(arrayAddFunc.capacity);
-        System.out.println(arrayAddFunc.size);
+        return a + b;
+    }
+
+    /**
+     * validate if plus result overflow
+     * @param a
+     * @param b
+     * @return
+     */
+    private boolean validatePlusOverflow(int a , int b) {
+        boolean positiveOverflow = a >= 0 && b >= 0 && a > (Integer.MAX_VALUE - b);
+        boolean negativeOverflow = a < 0 && b < 0 && (a < Integer.MIN_VALUE - b);
+
+        if (positiveOverflow || negativeOverflow) {
+            return Boolean.TRUE;
+        }
+        return Boolean.FALSE;
+    }
+
+
+
+    public static void main(String[] args) {
+
     }
 
 }
