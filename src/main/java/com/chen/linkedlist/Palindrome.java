0104d2405fe85e16297a88a1bfd7c1c43cde4a7e
javacxx <xx.chen@foxmail.com>
2019-09-29 16:57:49 +0800

    stack palindrome

--- src/main/java/com/chen/linkedlist/Palindrome.java
+++ src/main/java/com/chen/linkedlist/Palindrome.java
@@ -1,5 +1,7 @@
 package com.chen.linkedlist;
 
+import java.util.Stack;
+
 /**
  * @author CXX
  * @date 2019/9/29 15:05
@@ -11,20 +13,44 @@ public class Palindrome {
         singlyLinkedList.add("B");
         singlyLinkedList.add("E");
         singlyLinkedList.add("B");
-        singlyLinkedList.add("A");
+        singlyLinkedList.add("A1");
 
         System.out.println(validatePalindrome(singlyLinkedList));
     }
 
     public static boolean validatePalindrome(SinglyLinkedList<String> singlyLinkedList) {
-        int i = 0;
-        int lastIndex = singlyLinkedList.getSize() - 1;
+        int listSize = singlyLinkedList.getSize();
+
+        int secondHalfBeginIndex;
+        boolean oddFlag = true;
+        if (listSize % 2 == 0) {
+            oddFlag = false;
+            secondHalfBeginIndex = listSize / 2;
+        } else {
+            secondHalfBeginIndex = listSize / 2 + 1;
+        }
+
+
+
+        int index = 0;
+        Stack<String> stack = new Stack();
         do{
-            if (!singlyLinkedList.get(i).equals(singlyLinkedList.get(lastIndex - i))) {
-               return false;
+            if (oddFlag) {
+                if (index < secondHalfBeginIndex-1) {
+                    stack.add(singlyLinkedList.get(index));
+                } else if( index == secondHalfBeginIndex-1) {
+                } else if (!singlyLinkedList.get(index).equals(stack.pop())) {
+                    return false;
+                }
+            } else {
+                if (index < secondHalfBeginIndex) {
+                    stack.add(singlyLinkedList.get(index));
+                } else if (!singlyLinkedList.get(index).equals(stack.pop())) {
+                    return false;
+                }
             }
-            i++;
-        } while (2*i < lastIndex);
+            index++;
+        } while (index < listSize);
         return true;
     }
 
